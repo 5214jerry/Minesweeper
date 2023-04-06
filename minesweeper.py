a58cf88313ccd2669f20c2bb3c6a87aed607077c
5214jerry <kellylin325825@gmail.com>
2023-04-06 22:23:54 +0800

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,27 +1,23 @@
+from ctypes import sizeof
+from glob import glob
+from turtle import left
+from xml.sax.handler import property_encoding
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
 options=Options()
-PATH="chromedriver.exe"
+PATH="C:/Users/User/OneDrive/桌面/code/chromedriver.exe"
 
 driver = webdriver.Chrome(executable_path=PATH,chrome_options=options)
-driver.get("https://minesweeper.online/")
+driver.get("https://minesweeperonline.com/")
 driver.maximize_window()
-driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[1]/div[1]/div/ul[1]/li[2]/a/span').click()
-WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="level_select_13"]/span'))
-).click()
-time.sleep(2)
-driver.execute_script("window.scrollTo(0,50);")
-WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CLASS_NAME, 'start'))
-).click()
+
 finishedcell = set()
 board = list()
 readedcell = set()
@@ -30,13 +26,22 @@ for i in range(480):
 
 flags = set()
 clicks = set()
-table = driver.find_elements_by_class_name("cell")
+table = list()
+tmptable = driver.find_elements_by_class_name("square.blank")
+for i in range(480):
+    table.append(tmptable[i])
+
+table[0].click()
+time.sleep(1)
+randomopen = False
 while len(finishedcell) != 480:
+    time.sleep(0.4)
     # print(flags)
+
+    #插旗
     for index in flags:
         actionR = ActionChains(driver)
         actionR.context_click(table[index]).perform()
-        finishedcell.add(index)
         board[index] = 10
         for i in (-31,-30,-29,-1,1,29,30,31):
             if index+i>=480 or index+i<0:
@@ -46,83 +51,104 @@ while len(finishedcell) != 480:
             if index%30 == 0 and (i in (-31,-1,29)):
                 continue
             board[index+i] = board[index+i]-1
+
+    #採雷
     clicks = clicks - flags
     flags = set()
     for index in clicks:
+        # print("click="+str(index))
         table[index].click()
     clicks = set()
-    blocks =  driver.find_elements_by_class_name("hd_opened")
-    for block in blocks:
-        if block in readedcell:
-            continue
-        row = int(block.get_attribute("data-x"))
-        column = int(block.get_attribute("data-y"))
-        index = 30*column+row
-        bombnumber=int(block.get_attribute("class").replace("cell size24 hd_opened hd_type",""))
-        if bombnumber == 0:
-            finishedcell.add(index)
-        board[index] = board[index]-int(999)+bombnumber
-        readedcell.add(block)
-    
-    for index in range(0,480):
-        if index in finishedcell:
-            continue
-        block = board[index]
-        if block>=9:
-            continue
-        if block <= 0:
-            table[index].click()
-            finishedcell.add(index)
-            continue
-        uc = 0 #unopen_count
-        tmpflags = set()
-        sideopened = set()
-        for i in (-31,-30,-29,-1,1,29,30,31):
-            if index+i>=480 or index+i<0:
-                continue
-            if (index+1)%30 == 0 and (i in (-29,1,31)):
-                continue
-            if index%30 == 0 and (i in (-31,-1,29)):
-                continue
-            if board[index+i] >= 100 :
-                uc = uc+1
-                tmpflags.add(index+i)
-                continue
-            if board[index+i] <=9 and board[index+i]>=1 and i in(-30,-1,1,30):
-                sideopened.add(index+i)
-                continue
 
-        if uc == block:
-            flags = flags | tmpflags
-            continue
+    for i in range(0,9):
+        blocks =  driver.find_elements_by_class_name("open"+str(i))
+        # print(len(blocks))
+        for block in blocks:
+            if block in readedcell:
+                continue
+            idstr = block.get_attribute("id").split('_',1)
+            # print(idstr)
+            row = int(idstr[1])-1
+            column = int(idstr[0])-1
+            index = 30*column+row
+            bombnumber=block.get_attribute("class").replace("square ","")
+            print(bombnumber)
+            bombnumber=int(bombnumber.replace("open",""))
+            print("bombnumber="+str(bombnumber))
+            if bombnumber == 0:
+                finishedcell.add(index)
+            board[index] = board[index]-int(999)+bombnumber
+            readedcell.add(block)
         
-        sideopened = sideopened - finishedcell
-        if uc == block+1:
-            # print(sideopened)
-            for index2 in sideopened:
-                check = 0
-                fc2 = 0
-                uc2 = 0
-                tmpclicks = set()
+        for index in range(0,480):
+            if index in finishedcell:
+                continue
+            block = board[index]
+            if block>=9:
+                continue
+            if block <= 0:
                 for i in (-31,-30,-29,-1,1,29,30,31):
-                    if index2+i>=480 or index2+i<0:
+                    if index+i>=480 or index+i<0:
                         continue
-                    if (index2+1)%30 == 0 and (i in (-29,1,31)):
+                    if (index+1)%30 == 0 and (i in (-29,1,31)):
                         continue
-                    if index2%30 == 0 and (i in (-31,-1,29)):
+                    if index%30 == 0 and (i in (-31,-1,29)):
                         continue
-                    if index2+i in tmpflags:
-                        check = check+1
+                    if board[index+i] <= 10:
                         continue
-                    if board[index2+i] == 10:
-                        fc2 = fc2+1
-                        continue
-                    if board[index2+i] >= 100:
-                        uc2 = uc2+1
-                        tmpclicks.add(index2+i)
-                        continue
-                if board[index2]-1 <= 0 and check >=2:
-                    clicks = clicks | tmpclicks
+                    table[index+i].click()
+                    print("click="+str(index))
+                finishedcell.add(index)
+                continue
+            uc = 0 #unopen_count
+            tmpflags = set()
+            sideopened = set()
+            for i in (-31,-30,-29,-1,1,29,30,31):
+                if index+i>=480 or index+i<0:
                     continue
-                if board[index2]-1 == uc2 and check >=2 and uc == 2:
-                    flags = flags | tmpclicks
+                if (index+1)%30 == 0 and (i in (-29,1,31)):
+                    continue
+                if index%30 == 0 and (i in (-31,-1,29)):
+                    continue
+                if board[index+i] >= 100 :
+                    uc = uc+1
+                    tmpflags.add(index+i)
+                    continue
+                if board[index+i] <=9 and board[index+i]>=1 and i in(-30,-1,1,30):
+                    sideopened.add(index+i)
+                    continue
+
+            if uc == block:
+                flags = flags | tmpflags
+                continue
+            
+            sideopened = sideopened - finishedcell
+            if uc == block+1:
+                # print(sideopened)
+                for index2 in sideopened:
+                    check = 0
+                    fc2 = 0
+                    uc2 = 0
+                    tmpclicks = set()
+                    for i in (-31,-30,-29,-1,1,29,30,31):
+                        if index2+i>=480 or index2+i<0:
+                            continue
+                        if (index2+1)%30 == 0 and (i in (-29,1,31)):
+                            continue
+                        if index2%30 == 0 and (i in (-31,-1,29)):
+                            continue
+                        if index2+i in tmpflags:
+                            check = check+1
+                            continue
+                        if board[index2+i] == 10:
+                            fc2 = fc2+1
+                            continue
+                        if board[index2+i] >= 100:
+                            uc2 = uc2+1
+                            tmpclicks.add(index2+i)
+                            continue
+                    if board[index2]-1 <= 0 and check >=2:
+                        clicks = clicks | tmpclicks
+                        continue
+                    if board[index2]-1 == uc2 and check >=2 and uc == 2:
+                        flags = flags | tmpclicks
